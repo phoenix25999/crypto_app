1de076a1e355e4e52c61cb87aae2a309a6e9910f
phoenix25999 <vishesh42@gmail.com>
2021-10-15 13:25:33 +0530

    Updates

---
+++ precache-manifest.4bf81dc5607eacd9404fc07601d41328.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8966156a1a78bae751f6124d51115e4",
+    "url": "/crypto_app/index.html"
+  },
+  {
+    "revision": "6e1000b35d9707ba894e",
+    "url": "/crypto_app/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "f1fbdee4ff96ecbdb5e3",
+    "url": "/crypto_app/static/css/main.31a02981.chunk.css"
+  },
+  {
+    "revision": "6e1000b35d9707ba894e",
+    "url": "/crypto_app/static/js/2.e2b20fe7.chunk.js"
+  },
+  {
+    "revision": "f079ba4f3fe2a4cf26004adb08761d61",
+    "url": "/crypto_app/static/js/2.e2b20fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32031cafa0e873b2afdb",
+    "url": "/crypto_app/static/js/3.13f1c16f.chunk.js"
+  },
+  {
+    "revision": "f1fbdee4ff96ecbdb5e3",
+    "url": "/crypto_app/static/js/main.39c54d42.chunk.js"
+  },
+  {
+    "revision": "ce2bcab8287808204bd7",
+    "url": "/crypto_app/static/js/runtime-main.f455d528.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/crypto_app/static/media/cryptocurrency.88532732.png"
+  }
+]);
